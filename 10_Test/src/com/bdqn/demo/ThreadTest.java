59f804b81819d75af3df76a70ba9bdd281080b4b
bxl_glb <goodMorning_pro@at.bdqn.com>
2021-04-28 15:54:18 +0800

    我加了个多线程测试类

---
+++ 10_Test/src/com/bdqn/demo/ThreadTest.java
@@ -0,0 +1,14 @@
+package com.bdqn.demo;
+
+import javax.servlet.annotation.WebServlet;
+
+/**
+ * @author chao
+ * @create 2021-04-27 17:13
+ */
+@WebServlet
+public class ThreadTest {
+    public static void main(String[] args) {
+        System.out.println(666);
+    }
+}
